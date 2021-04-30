24ee7194d8c5b22b7ab00c9b188cfdbfb7fb9bae
junghwa <junghwa1996@naver.com>
2021-05-01 01:02:21 +0900

    셋팅

---
+++ src/js/common.js
@@ -0,0 +1,18 @@
+
+var pc_device = "win16|win32|win64|mac|macintel";
+
+// 접속한 디바이스 환경
+var this_device = navigator.platform;
+
+if (this_device) {
+
+  if (pc_device.indexOf(navigator.platform.toLowerCase()) < 0) {
+    console.log('MOBILE');
+  } else {
+    console.log('PC');
+    alert('모바일 환경에서만 실행 가능합니다.');
+    var body = document.getElementById("wrap");
+    body.style.display = "none";
+  }
+
+}
